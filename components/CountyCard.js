37bbdedb1c275e40a26a9698ef895c1a10a7df68
Mike Bussert <mbussert@gmail.com>
2021-09-22 22:34:13 -0400

    Adjusted shadow on cards

--- components/CountyCard.js
+++ components/CountyCard.js
@@ -5,7 +5,7 @@ function CountyCard() {
     <div className="max-w-6xl mx-auto">
       <div className="flex items-center justify-center">
         <div className="max-w-sm w-full sm:w-full lg:w-full py-6 px-3">
-          <div className="bg-white shadow-xl rounded-md overflow-hidden">
+          <div className="bg-white shadow-md rounded-md overflow-hidden">
             <div className="bg-cover bg-center content-div image-cover h-56 p-4"></div>
             <div className="p-5 pl-7 pb-12">
               <p className="tracking-wide text-xl font-bold text-gray-700 pb-3">
